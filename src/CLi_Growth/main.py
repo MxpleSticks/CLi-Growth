5b18ddf448813d359df8cf8f095bbd3896bd419e
MxpleSticks <blf25@icloud.com>
2026-07-11 14:29:29 -0400

    Setting stuff up and starting main.py

---
+++ src/CLi_Growth/main.py
@@ -0,0 +1,34 @@
+import rich
+import yfinance as yf
+
+print("======================")
+print("Welcome to CLi Growth!")
+print("======================")
+print("<This is a simple CLI tool to help you calculate stock exponential growth>\n")
+print("Please enter the following details to calculate your stock growth:")
+
+while True:
+    try:
+        initialInvestment = float(input("1. Initial Investment Amount (in USD): "))
+        if initialInvestment < 0:
+            print("Invalid input. Please enter a valid number for the Initial Investment Amount (in USD): ")
+            continue
+        break
+    except ValueError:
+        print("Invalid input. Please enter a valid number for the Initial Investment Amount (in USD): ")
+
+while True:
+    try:
+        stockSymbol = input("2. Stock symbol (e.g., AAPL, TSLA):")
+        if not stockSymbol:
+            print("Invalid input. Please enter a valid stock symbol (e.g., AAPL, TSLA): ")
+            continue
+
+        print(f"Verifying stock symbol '{stockSymbol}'...")
+        tickerData = yf.Ticker(stockSymbol)
+        try:
+            
+
+
+monthlyContribution = float(input("3. Monthly Contribution Amount (in USD):"))
+compoundFrequency = input("4. Compound Frequency (e.g., monthly, quarterly, annually):")
