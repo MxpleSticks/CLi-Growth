d712012aeb7a01e2652eccd968609136b6e9d31d
MxpleSticks <blf25@icloud.com>
2026-07-14 09:25:41 -0400

    making the interface rich 🤑 pt3

--- src/CLi_Growth/display.py
+++ src/CLi_Growth/display.py
@@ -2,6 +2,7 @@ import plotext as plt
 from rich.console import Console, Group
 from rich.panel import Panel
 from rich.table import Table
+from rich.text import Text
 
 console = Console()
 
@@ -17,13 +18,42 @@ def showResults(portfolioAssets, finalBalance, yearlyData):
     growthTable.add_column('Year')
     growthTable.add_column('Balance', justify='right')
 
+    years = []
+    balances = []
+
     for i in yearlyData:
         growthTable.add_row(str(i['year']), f"${i['balance']:,.2f}")
-
-    sideBySideGrid = Table.grid(padding=6)
-    sideBySideGrid.add_row(allocTable, growthTable)
-
-    content = Group(
-        f"[bold green]Final Balance: ${finalBalance:,.2f}[/bold green]", sideBySideGrid)
+        years.append(i['year'])
+        balances.append(i['balance'])
     
-    console.print(Panel(content, title="[bold blue]Portfolio Growth Summary[/bold blue]", width=70))
+    innerGrid = Table.grid(padding=6)
+    innerGrid.add_row(allocTable, growthTable)
+
+    content = Group(f"[bold green]Final Balance: ${finalBalance:,.2f}[/bold green]", innerGrid)
+    
+    summaryPanel = Panel(content, title="[bold blue]Portfolio Growth Summary[/bold blue]", width=70)
+
+    plt.clf()
+    plt.plot(years, balances, marker='hd')
+    plt.title("Portfolio Growth")
+
+    low = min(balances)
+    high = max(balances)
+    step = (low + high) / 4
+
+    ticks = [low, low + step, low + (2 * step), low + (3 * step), high]
+    labels = [f"${int(x):,}" for x in ticks]
+
+    plt.yticks(ticks, labels)
+
+
+    plt.plotsize(60,20)
+    plt.theme('clear')
+
+    plotAnsiString = plt.build()
+    plotRenderable = Text.from_ansi(plotAnsiString)
+
+    masterGrid = Table.grid(padding=4)
+    masterGrid.add_row(summaryPanel, plotRenderable)
+
+    console.print(masterGrid)
