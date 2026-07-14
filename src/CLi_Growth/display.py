e6dd9b396e62967829a50f06d190810654432576
MxpleSticks <blf25@icloud.com>
2026-07-13 23:55:44 -0400

    making the interface rich 🤑 pt2

--- src/CLi_Growth/display.py
+++ src/CLi_Growth/display.py
@@ -1,16 +1,29 @@
-from rich.console import Console
+import plotext as plt
+from rich.console import Console, Group
 from rich.panel import Panel
 from rich.table import Table
-from rich.columns import Columns
 
 console = Console()
 
 def showResults(portfolioAssets, finalBalance, yearlyData):
-    print("-- Portfolio Growth Summary --")
-    print("your final balance is: ${}".format(round(finalBalance, 2)))
-    
+    allocTable = Table(show_header=True, header_style='bold cyan')
+    allocTable.add_column('symbol')
+    allocTable.add_column('Weight', justify='right')
+
     for i in portfolioAssets:
-        print('- {}: {}%'.format(i['symbol'], i['weight'] * 100))
+        allocTable.add_row(i['symbol'], f"{i['weight'] * 100:.0f}")
+
+    growthTable = Table(show_header=True, header_style="bold magenta")
+    growthTable.add_column('Year')
+    growthTable.add_column('Balance', justify='right')
 
     for i in yearlyData:
-        print('Year {}: ${}'.format(i['year'],round(i['balance'], 2)))
+        growthTable.add_row(str(i['year']), f"${i['balance']:,.2f}")
+
+    sideBySideGrid = Table.grid(padding=6)
+    sideBySideGrid.add_row(allocTable, growthTable)
+
+    content = Group(
+        f"[bold green]Final Balance: ${finalBalance:,.2f}[/bold green]", sideBySideGrid)
+    
+    console.print(Panel(content, title="[bold blue]Portfolio Growth Summary[/bold blue]", width=70))
